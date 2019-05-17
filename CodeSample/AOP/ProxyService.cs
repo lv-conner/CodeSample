10dcd3cbb320c61626175f2847cc8c0525bc2b6b
lv-conner <lvmh123456@gmail.com>
2019-05-17 12:29:17 +0800

    aop

---
+++ CodeSample/AOP/ProxyService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSample.AOP
+{
+    public class ProxyService : ISayHello
+    {
+        public void SayHello(string name)
+        {
+            var context = new InterceptroContext();
+
+        }
+    }
+    public class CNSayHello : ISayHello
+    {
+        public void SayHello(string name)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+    public interface ISayHello
+    {
+        [Interceptor(typeof(LogInterceptor))]
+        void SayHello(string name);
+    }
+    public class InterceptroContext
+    {
+
+    }
+    public interface IInterceptor
+    {
+        Task Invoke(InterceptroContext context, Func<InterceptroContext,Task> next);
+    }
+    public class LogInterceptor : IInterceptor
+    {
+        public async Task Invoke(InterceptroContext context, Func<InterceptroContext,Task> next)
+        {
+            Console.WriteLine("begin");
+            await next(context);
+            Console.WriteLine("end");
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface | AttributeTargets.Method)]
+    public class InterceptorAttribute:Attribute
+    {
+        public Type InterceptorType { get; private set; }
+        public InterceptorAttribute(Type interceptorType)
+        {
+            InterceptorType = interceptorType;
+        }
+    }
+}
--- CodeSample/Program.cs
+++ CodeSample/Program.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using AspectCore.Extensions.Reflection;
 using CodeSample.ExpressionTree;
 using CodeSample.ExpressionTree.Test;
 using CodeSample.PipelineProgram;
+using System.Linq;
 
 namespace CodeSample
 {
@@ -9,8 +14,29 @@ namespace CodeSample
     {
         static void Main(string[] args)
         {
-            //Tree.Convert<Person, PersonDto>(new Person());
-            Sample.Case();
+            var ctinfo = typeof(Person).GetConstructor(new Type[] { });
+            var constructorInfo = typeof(Person).GetTypeInfo().GetConstructor(new Type[0]);
+            var reflector = constructorInfo.GetReflector();
+            
+            var aa = new object[] { };
+            List<TimeSpan> times = new List<TimeSpan>();
+            //Tree.Convert<Person, Pnew object[] { }ersonDto>(new Person());
+            for (int i = 0; i < 1000000; i++)
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                var instance = ctinfo.Invoke(aa);
+                //var instance = reflector.Invoke(aa);
+                //var instance = new Person();
+                stopwatch.Stop();
+                //Console.WriteLine(stopwatch.Elapsed);
+                times.Add(stopwatch.Elapsed);
+            }
+
+            Console.WriteLine(times.Average(p => p.Ticks));
+
+
+            //Sample.Case();
             Console.ReadKey();
             Console.WriteLine("Hello World!");
         }
