f612e9970734afb918681e0a8418e37045f9d9b3
lv-conner <lvmh123456@gmail.com>
2019-07-01 12:21:52 +0800

    private field

---
+++ ConsoleAppBasic/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace ConsoleAppBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person("tim lv");
+            var props = p.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public);
+
+            var fields = p.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (var field in fields)
+            {
+                field.SetValue(p, "haha");
+                Console.WriteLine(field.Name);
+            }
+            Console.WriteLine(p);
+
+            Console.ReadKey();
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Person
+    {
+        private string id;
+        public Person(string Id)
+        {
+            id = Id;
+        }
+        public override string ToString()
+        {
+            return id;
+        }
+    }
+}
